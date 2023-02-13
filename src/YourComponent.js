6ce9ad05ff29a30405a0ac8aa6163333fff16bee
Memo <gbastoa17@gmail.com>
2023-02-12 20:09:07 -0600

    Se pudo arreglar el problema de la API y el mapa ya carga

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-
+import GoogleMaps from 'simple-react-google-maps'
 /*
 * Use this component as a launching-pad to build your functionality.
 *
 */
-export default class YourComponent extends Component {
 
+export default class YourComponent extends Component {
   render() {
     return (
-      <div style={divStyle}>
-		  <h1> Put your solution here!</h1>
-      </div>
+      <GoogleMaps
+      apiKey={"AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A"}
+      style={{height: "400px", width: "100%"}}
+      zoom={6}
+      center={{lat: 37.4224764, lng: -122.0842499}}
+      markers={{lat: 37.4224764, lng: -122.0842499}} //optional
+      />
     );
   }
 }
-
-var divStyle = {
-  border: 'red',
-  borderWidth: 2,
-  borderStyle: 'solid',
-  padding: 20
-};
