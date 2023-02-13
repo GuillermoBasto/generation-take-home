98fa1d48ee7d4558bdb9b38060c07b7e4046781e
Memo <gbastoa17@gmail.com>
2023-02-13 16:54:20 -0600

    se intentó agregar todos los pines solicitados sin exito hasta el momento

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -5,16 +5,55 @@ import GoogleMaps from 'simple-react-google-maps'
 *
 */
 
+function getMarkers(){
+  fetch("./store_directory.json").then(function(response) {return response.json();}).then( function(data){
+      console.log(data);
+      let direccion = data[0].Address;
+      let inicioURL = "https://maps.googleapis.com/maps/api/geocode/json?address=$"
+      let URL = inicioURL+direccion.split(" ").join("+")
+  
+      URL += "&key=AIzaSyCRrdnhmnojDV5GPXFOtGeKObf0fiMmM3E"
+      console.log(URL);
+      
+      let resp = fetch(URL).then(function(response){return response.json();}).then(function(location){
+        let resp3 = createMark(location);
+        console.log(resp3);
+        return resp3;
+      })
+      console.log(resp);
+    });
+}
+
+function createMark(location){
+  let latitud, longitud;
+  console.log(location);
+  latitud = location.results[0].geometry.location.lat
+  console.log(latitud);
+  longitud = location.results[0].geometry.location.lng
+  console.log(longitud);
+  let newmark = {lat: latitud, lng:longitud}
+  return newmark;
+}
+
 export default class YourComponent extends Component {
   render() {
     return (
       <GoogleMaps
-      apiKey={"AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A"}
-      style={{height: "400px", width: "100%"}}
-      zoom={6}
-      center={{lat: 37.4224764, lng: -122.0842499}}
-      markers={{lat: 37.4224764, lng: -122.0842499}} //optional
+      apiKey={"AIzaSyCRrdnhmnojDV5GPXFOtGeKObf0fiMmM3E"}
+      style={{height: "600px", width: "100%"}}
+      zoom={12}
+      center={{lat: 19.432608, lng: -99.133209}}
+      markers={getMarkers()}
       />
     );
   }
 }
+
+//////////////////////////COMENTARIOS////////////////////
+/// Logre obtener los datos para los marcadores desde el archivo store_directory.json
+/// para posteriormente mandarlos como un a url al servicio de Geocoding de la API de 
+/// Google Maps, de esa petición logre recibir la respuesta y aislar la latitud y
+/// longitud correspondientes a la direccion dada en el json, lo que no pude hacer es
+/// pasar ese objeto que al final imprimo por consola hasta el componente Map que
+/// debería recibir ese objeto y usarlo con la propiedad markers para crear todos los
+/// pines o marcadores solicitados en el ejercicio, por ello es que solo carga el mapa
